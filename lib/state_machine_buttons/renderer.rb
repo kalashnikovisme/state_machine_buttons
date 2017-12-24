e5a389ff05ef338fca4ebbc57f177ee1c8243000
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-12-24 15:22:58 +0400

    Add route namespace

--- lib/state_machine_buttons/renderer.rb
+++ lib/state_machine_buttons/renderer.rb
@@ -2,23 +2,24 @@
 
 module StateMachineButtons
   module Renderer
-    def state_events_buttons(object, state_method: :state, url: nil, without: nil)
+    def state_events_buttons(object, state_method: :state, url: nil, without: nil, namespace: nil)
       model_name = object.model.model_name.name.underscore
-      excepted_actions = without.is_a?(Array) ? without.map(&:to_sym) : [without.to_sym]
+      excepted_actions = without.is_a?(Array) ? without.map(&:to_sym) : [without.to_sym] if without
       transitions = object.model.send("#{state_method}_transitions").reject do |t|
-        excepted_actions.include? t.event
+        excepted_actions.present? && excepted_actions.include?(t.event)
       end
       content_tag(:div, class: 'btn-group-vertical') do
         transitions.each do |event|
-          button event: event, model_name: model_name, object: object, state_method: state_method, url: url
+          button event: event, model_name: model_name, object: object, state_method: state_method, url: url, namespace: namespace
         end
       end
     end
 
     private
 
-    def button(event:, model_name:, object:, state_method:, url:)
-      href = url || send("admin_#{model_name}_path", object.model, model_name => { state_method => event.to })
+    def button(event:, model_name:, object:, state_method:, url:, namespace:)
+      route_method_name = "#{namespace.present? ? "#{namespace}_": ""}#{model_name.gsub('/', '_')}_path"
+      href = url || send(route_method_name, object.model, model_name => { state_method => event.to })
       concat(
         link_to(
           href,

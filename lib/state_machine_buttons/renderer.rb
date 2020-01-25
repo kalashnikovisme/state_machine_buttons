c439f199778594643dab6cc6ea641cc90fe5c3b0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-26 03:07:51 +0400

    Refactored buttons with static buttons and event based updating

--- lib/state_machine_buttons/renderer.rb
+++ lib/state_machine_buttons/renderer.rb
@@ -2,7 +2,7 @@
 
 module StateMachineButtons
   module Renderer
-    def state_events_buttons(object, state_method: :state, route_method: nil, parameters: {}, without: nil, namespace: nil, model_param_name: nil)
+    def state_events_buttons(object, state_method: :state, controller:, action:, parameters: {}, without: nil, namespace: nil, model_param_name: nil, button_options: {})
       model_name = object.model.model_name.name.underscore
       model_param_name ||= model_name
       excepted_actions = without.is_a?(Array) ? without.map(&:to_sym) : [without.to_sym] if without
@@ -11,23 +11,38 @@ module StateMachineButtons
       end
       content_tag(:div, class: 'btn-group-vertical') do
         transitions.each do |event|
-          button event: event, model_name: model_name, object: object, state_method: state_method, route_method: route_method, namespace: namespace, parameters: parameters, model_param_name: model_param_name
+          button(
+            event: event.event,
+            model_name: model_name,
+            object: object,
+            state_method: state_method,
+            controller: controller,
+            action: action,
+            namespace: namespace,
+            parameters: parameters,
+            model_param_name: model_param_name,
+            form_options: button_options
+          )
         end
       end
     end
 
     private
 
-    def button(event:, model_name:, object:, state_method:, route_method:, namespace:, parameters:, model_param_name:)
-      route_method ||= "#{namespace.present? ? "#{namespace}_": ""}#{model_name.gsub('/', '_')}_path"
-      href = send route_method, object.model, parameters.merge(model_param_name => { state_method => event.to })
+    def button(event:, model_name:, object:, state_method:, controller:, action:, namespace:, parameters:, model_param_name:, form_options:)
+      attributes = { "#{state_method}_event" => event }
       concat(
-        link_to(
-          href,
-          class: "btn btn-sm btn-xs btn-#{object.send("#{state_method}_button_color", event.event)}",
-          method: :patch
+        patch_button(
+          record: object.model,
+          controller: controller,
+          action: action,
+          parameters: parameters,
+          attributes: attributes,
+          model_name: model_param_name,
+          button_options: { class: "btn btn-sm btn-xs btn-#{object.send("#{state_method}_button_color", event)}" },
+          form_options: form_options
         ) do
-          object.class.send "human_#{state_method}_event_name", event.event
+          object.class.send "human_#{state_method}_event_name", event
         end
       )
     end

4686d26cc7e70a984f01fc7da47580031031b5d3
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-02-02 08:09:57 +0400

    Many actions for removing state_machien

--- lib/state_machine_buttons/renderer.rb
+++ lib/state_machine_buttons/renderer.rb
@@ -6,13 +6,13 @@ module StateMachineButtons
       model_name = object.model.model_name.name.underscore
       model_param_name ||= model_name
       excepted_actions = without.is_a?(Array) ? without.map(&:to_sym) : [without.to_sym] if without
-      transitions = object.model.send("#{state_method}_transitions").reject do |t|
-        excepted_actions.present? && excepted_actions.include?(t.event)
+      transitions = object.model.aasm(state_method).permitted_transitions.reject do |t|
+        excepted_actions.present? && excepted_actions.include?(t[:event])
       end
       content_tag(:div, class: 'btn-group-vertical') do
         transitions.each do |event|
           button(
-            event: event.event,
+            event: event[:event],
             model_name: model_name,
             object: object,
             state_method: state_method,
@@ -42,7 +42,8 @@ module StateMachineButtons
           button_options: { class: "btn btn-sm btn-xs btn-#{object.send("#{state_method}_button_color", event)}" },
           form_options: form_options
         ) do
-          object.class.send "human_#{state_method}_event_name", event
+          binding.pry if state_method == :participation_state
+          I18n.t("state_machines.#{object.model.class.name.underscore}.#{state_method}.events.#{object.model.aasm(state_method).events.select { |ev| ev.name == event }.first}")
         end
       )
     end
